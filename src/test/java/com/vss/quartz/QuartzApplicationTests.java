3128556452aaf8d749fb249e4e0003cd1741281f
trungkien <kienn7454@gmail.com>
2025-12-17 09:07:01 +0700

    Initial commit

---
+++ src/test/java/com/vss/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vss.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
